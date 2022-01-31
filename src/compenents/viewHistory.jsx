410d3bf58957000bae9361957ef4fdffc65b393b
TheEpicPebbles <40904349+TheEpicPebbles@users.noreply.github.com>
2022-01-30 20:43:41 -0500

    Copyright Information
    Don't tread on me

--- src/compenents/viewHistory.jsx
+++ src/compenents/viewHistory.jsx
@@ -1,3 +1,134 @@
+/**
+ *       :::::::::  :::::::::   ::::::::  :::    ::: :::::::::::   :::   :::   ::::::::::: ::::::::::: :::   :::    
+	 :+:    :+: :+:    :+: :+:    :+: :+:    :+:     :+:      :+:+: :+:+:      :+:         :+:     :+:   :+:     
+	+:+    +:+ +:+    +:+ +:+    +:+  +:+  +:+      +:+     +:+ +:+:+ +:+     +:+         +:+      +:+ +:+       
+   +#++:++#+  +#++:++#:  +#+    +:+   +#++:+       +#+     +#+  +:+  +#+     +#+         +#+       +#++:         
+  +#+        +#+    +#+ +#+    +#+  +#+  +#+      +#+     +#+       +#+     +#+         +#+        +#+           
+ #+#        #+#    #+# #+#    #+# #+#    #+#     #+#     #+#       #+#     #+#         #+#        #+#            
+###        ###    ###  ########  ###    ### ########### ###       ### ###########     ###        ###             
+	  ::::::::   ::::::::  :::::::::: ::::::::::: :::       :::     :::     :::::::::  ::::::::::                
+	:+:    :+: :+:    :+: :+:            :+:     :+:       :+:   :+: :+:   :+:    :+: :+:                        
+   +:+        +:+    +:+ +:+            +:+     +:+       +:+  +:+   +:+  +:+    +:+ +:+                         
+  +#++:++#++ +#+    +:+ :#::+::#       +#+     +#+  +:+  +#+ +#++:++#++: +#++:++#:  +#++:++#                     
+		+#+ +#+    +#+ +#+            +#+     +#+ +#+#+ +#+ +#+     +#+ +#+    +#+ +#+                           
+#+#    #+# #+#    #+# #+#            #+#      #+#+# #+#+#  #+#     #+# #+#    #+# #+#                            
+########   ########  ###            ###       ###   ###   ###     ### ###    ### ##########                      
+					::::::::  :::::::::   ::::::::  :::    ::: :::::::::                                                       
+				  :+:    :+: :+:    :+: :+:    :+: :+:    :+: :+:    :+:                                                       
+				 +:+        +:+    +:+ +:+    +:+ +:+    +:+ +:+    +:+                                                        
+				:#:        +#++:++#:  +#+    +:+ +#+    +:+ +#++:++#+                                                          
+			   +#+   +#+# +#+    +#+ +#+    +#+ +#+    +#+ +#+                                                                 
+			  #+#    #+# #+#    #+# #+#    #+# #+#    #+# #+#                                                                  
+			  ########  ###    ###  ########   ########  ###     
+																																						   WWNK0OkxkOXW  
+																																					 WXKOxdolc::;;;;:cd0W
+																																				WNKkdl:;,,;;:::::::::;;cO
+																																		   WN0xolc:;;:ccclllllllcccc::;:x
+																																	   WN0xlc;;::clllllllcllllllllcc::;:k
+																																   WX0xoc:::clllcc::::;;;,,',;clllcc::;oX
+																																N0xoc::clllcccccccclloooool:'.,cllc::;oKW
+																															WKOdl::clccccccllooooooooodddddo:.'clc::;l0W 
+																														WX0xoc:ccccccclloooooooooooooooooddo:';cc:::oKW  
+																													 WXOdlcccccccllooooooooooooloooooooooddl,,cc::ckN    
+																												  N0xolcccccclloooooolllllllllllloooooooddl;;cc::lOW     
+																											   N0d:..,cccllooooolllccclllcccccclllooooodol;;cc::oKW      
+																		  WWWNNNNXXXXXXXXNNNWW              N0d:.. ...:oooolllcclldxk0KKK0kocccclloooodoc;:c::oON        
+																 WWNXK0OkxddoolllccccccccllloodxxkO0XNW  W0o;... .....,llcccodkOKNW        0occcllooodl:;:c:lkN          
+														  WWWXK0kxdolc:::::::::::ccccc:::::::::::;::clodxd;....  .....'coxOKNW             Koccllooooc::c::oKW           
+													 WWWNKOkdocc::cccccccccccccccccccccccccccccccccc::::;;;,,'.....;cdOXW                WKxlclloool::c:;:xX             
+												  WWNKOxdlc::ccccccccccccccllllccccccccccccccllccccccccccc:::;;,';o0N                   WOocllooolc::c:;o0W              
+											  WWWX0kdlccccccccccccccccccccllllllllccccccccccllllcccccccccccccc:::;;cokKW               Xxlllooooc::c::lON                
+											WWX0kdlccccccccccccccccccccccccllllllllcccccccccllllccccccllllllcccccc::;;:okXW          W0oclooool::c:;lkN                  
+										  WNKOdlccccccccccccccllllcccccccccclllllccccccccccccccccccccccllllllccccccccc:;;:oON      WXxlllooolccc:;cxX                    
+									   WWX0kolccccccccccclllllllllcccccccccccccccccccccccccccccccccccccllllllllcllllcccc::;;lkXW WXkollooolccc:;cxX                      
+									  WX0xolccccccccccclllllllllccccccc:::::::::::::::::::::::::::ccccccccccccllllllllcccc::;;cxKOollooolccc::lkXW                       
+									WX0xolcccccccclllllllccccccccc:::::::::;;;;;;;;;;;;;;;;;;;;;:::::ccccccccccclllllcccccccc:;:cllooolccc:;ckX                          
+								  WN0kdlcccccclllllcccccccccccc::::;;;;;;;;,,,,,,,,,,,,,,,,,,,,,;;;;;::::::cccccccccllccccccccc:::lllccc:,;xX                            
+								WNKOdllcccccccllllllccccccc::::;;;;;,,,,,,''''''''''''''''''''''',,,,;;;;;::::ccccccccclllccccccc::::c;,;oKW                             
+							   WX0kolccccccccclllcccccccc:::;;;;,,,,'''''........................''''',,,;;;;::::cccccccllcccccccc::;,,oKW                               
+							  WXOxolcccccccccccccccccc::::;;,,,,''''.................................'''',,,,;;;:::ccccccccccccccccc:;;oX                                
+							 NKkdllccccccccccccccccc:::;;;,,,'''..........................................''',,;;;:::cccccccccccccccc:;;l0W                              
+							N0kdllcccccclllcccccccc::;;;,,'''...............................................''',,,;;;::ccccccccllllccc:;;cOW                             
+						   N0kdllccccclllllcccccc:::;;,,'''...................................................'''',,;;::cccccccllllccccc:;:OW                            
+						  N0kdllccccccllllccccc:::;;;,,''...............................'''''....................'',,;;:::ccccccllllllcc::;:OW                           
+						 WKkdolcccccccllccccc:::;;;;,,''....................................',,,,'.................'',,;;:::ccccclllllccc:;,c0W                          
+						WKOxolcccccccllccccc:::;;,,,''..........................................',;,'...............'',,;;:::ccccclllllccc:;;oX                          
+					   WXOxdlccccllllllcccc:::;;,,,''.........................''''.................';;,'............''',,;;:::cccclllllcccc:;;xN                         
+					   W0kdolccccllllllccc:::;;,,,''..........................''''........'..........';:,'...........''',,;;;::cccllllcccccc:;c0                         
+					  WXOxdlccccclllllcccc::;;;,,''................................''',,,,,,''.........,:;'............'',,;;::ccccllccccccc:;;dN                        
+					  W0kdolcccccclllcccc:::;;,,''...............'...........''',,,,,,;,,;;:;,'.........'::,............'',,;::cccccccccccccc:;c0                        
+					  NOxdocccccccccccccc::;;,,''..............''.........',;;::cccllccc::;,,;;;'.........;:,...........'',,;;::cccccclcccccc:;;xW                       
+					 WXkxdlcccccccccccccc::;;,,''.............',.........,::ccllooodddddddol:,,;:;'........::'...........'',,;;::cccclllccccc:;,lX                       
+					 WKkxolccccccccccccc::;;,,''..............,'........;cclllodoolllllcloxxdl;,,:;'.......':;'...........'',;;::cccccllllcccc:,c0                       
+					 N0xdolccccccccccccc::;;,,''.............,,........,cllllodo:,,;;;;;;::okxl;';:;'.......;:,...........'',,;:::cccccccccccc:;:O                       
+					 NOxdolccccccccccccc::;;,'''.............,,.......'collcldd:,''''',,,;::dkdc,,::'.......,:,...........'',,;::ccccccccccccc:;;kW                      
+					 NOxdolccccccccccccc::;;,'''.............;,.......,lollcoxo;'''',',,,;:;lkxl;';:'.......,:,..........''',,;::cccccllcccccc:;;kW                      
+					 NOxxolccccccccccccc::;;,'''.............;,.......'lollclkxc,,,,,,,,,;:cdkdl;,;;'.......,:,..........''',,;::cccllllcccccc:;;kW                      
+					 N0xxdlccccccccccccc::;;,'''.............,;...'''..:oolccoxdc;;::;;;cccoxxl:,,;,'.......;;'...........'',,;;:cclllllcccccc:;:O                       
+					 W0kxdllcccccccccccc::;;,,''.............';'........:oollcloodoodolloodddl:,,;,'.......';,...........''',;;::ccllllllccccc:;c0                       
+					 WKkxdolcccclllllccc::;;,,''..............,;'........;loolllloooooooodolc;,,,'.........,;'...........'',,;;::ccllllllccccc:;oX                       
+					  XOxdolccccllllllccc::;;,,''..............;;'........';cllllllllllllc::;,''..........,,'............'',,;;:ccccccllllcccc:;xW                       
+					  N0xxdlcccclllllcccc::;;,,,''..............;:,..........,;::::::::;;,,'''...........','............'',,;;:ccccclllcccccc:;c0                        
+					  WXkxdocccccccccccccc::;;;,,'...............,;,..............''''''''''............'''............'',,;;::cccccccccccccc:;dN                        
+					   N0xxdlcccccccccccccc::;;,,''................','................................'''.............'',,;;::cccccccccccccc:;c0                         
+						XOxxolcccccccllccccc::;;,,'''................',,,'...........................................'',,;;:::cccccllllcccc:;;xN                         
+						WKkxdolccccccllccccc:::;;,,'''..................'',''.......................................'',,;;:::cccccllllcccc::;oX                          
+						 N0kxdlcccccccllccccc:::;;,,'''............................................................'',,;;:::cccccccccccccc:;l0W                          
+						  NOxxdlcccccccllcccccc:::;;,,'''........................................................''',,;;::cccccccclcccccc:;c0W                           
+						  WXOdddlccccclllllcccccc::;;;,,''.....................................................''',,;;::ccccccclllllcccc:;cOW                            
+						   Nxccodlccccclllllccccccc::;;,,,'''...............................................'''',,,;;::cccccccllllllccc:;cOW                             
+						 W0dl:;codolcccccclcccccccccc::;;,,,'''.........................................''''',,,;;;:::ccccccccccccclcc::l0W                              
+						Xklcccl::ldolcccccccccccccllcc:::;;;,,,''''...................................''',,,,;;;;:::ccccccccccccccccc::dX                                
+					  NOolccloll::dxdolccccccccccllllccc::::;;;;,,,''''...........................'''',,,;;;;::::cccccllccccccccccc::ckN                                 
+					WXxlc:coolccd00kxdolcccccccclllllccccccc::::;;;,,,,''''''''''''''''''''''''''',,,,;;;;::::cccclllllllcccccccc:::dKW                                  
+				   NOlc:cloolccxN WKOkxdolccccclllllllccccccccc:::;;;;;,,,,,,,,,,,,,,,,,,,,,,,,,,,;;;;:::::ccccccllllllccccccccc::lON                                    
+				 WKdlc:cooolclOW    NKkxddolccccccllccccccccccccc:::::::::;;;;;;;;;;;;;;;;;;;;;;;:::::cccccccccclllccccccccccc::lkXW                                     
+				Xxlc::loollcl0W      WN0kxddolcccccccccccccccccccccccccc::::::::::::::::::::::::::cccccccccccccclccccccccccc::lkXW                                       
+			  W0occ:cooolc:cOW         WNKOxxdolc:cclcccccccccccccccccccccccccccccccccccccccccccccccccccclllllccccccccccc::coOXW                                         
+			 Xxccc:cooolc::kW            WNKOdlccllllllcccccccccllllccccccccccccccccccccccccccccccccccccllllllccccccccc::lx0N                                            
+		   WKo:c::looolc:,cK              NKxl::clolcllc:ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc::ldOXW                                              
+		  WOl:c::looollc,.lX          WN0kdlllolccccc:,,;cccccccccccccccccccccccccccccccccccclllcccccccccccccccc::cldOXW                                                 
+		 Xxc:c:cooolllc:'.,kN    WWX0kdolllooolcccc;,;:oddollccccccccccccccccccccccccccccccccllcccccccccccccc:clox0XW                                                    
+	   W0o:::;cooolllcc:'..'codxddolccllooollcccc;;ckXXX0Okxddoollllcccccccccccccccccccccccccccccccccccccclodk0XNW                                                       
+	  WOc;::;codoolllccc;,''',,;:cllllooolccll:;cd0N    WWNXK0OkxxddoollllcllllccccccccccccccccccccclodxkOKXWWW                                                          
+	 Nkc;c:;:odoooollccccccccclllllooolccclc;coOX            WWNNXK00OkkxxddddoooolllllllooooddxxkO0KXNWWW                                                               
+	Nx:;c:,:odooooolllllllllllllooolccccc:cokXW                     WWWNNNXXXKKKKK00000KKKXXXNNNWWWW                                                                     
+   Nx:;:c;;ldoooolooollllloooooolcccccccoxKW                                                                                                                             
+  Nd:;:c:':ooooooooooooooooollcccllc:lxKW                                                                                                                                
+ Nx;;:cc,'cdooooooooooooolccccclccox0NW                                                                                                                                  
+Wk:;::cc,'codddoddooolcc:ccllccox0N                                                                                                                                      
+Kl;;:ccc,.';cllccc::::ccllccokKNW                                                                                                                                        
+k:;::cclc;,,,,;;;:clllllodkKN                                                                                                                                            
+x:;::cclllllllllllccldkKNW                                                                                                                                               
+k:;:::ccccclccccldk0XW                                                                                                                                                   
+Xkl:::cccclodxO0XW                                                                                                                                                       
+ WXK00000KXNW                                                                                                                                                            
+																																																																																			 
+																														   
+																																		.oOOo. .oOOo. .oOOo. .oOOo. 
+																																			 O O    o      O      O 
+																																			 o o    O      o      o 
+																																			O' o    o     O'     O' 
+																																		   O   O    O    O      O   
+																																		 .O    o    O  .O     .O    
+																																		oOoOoO `OooO' oOoOoO oOoOoO 
+
+-- javascript/typescript/react Source Code © Proximity Software Group (2016-2022)
+
+-- This file is original and proprietary. Unless you have been granted a legal license,
+-- you have no legal right to view or use any part of this code. Violations or removal of any
+-- copyright notices, or any text herein constitutes copyright infringement and lead to prosecution.
+-- Intercepting a copy through a breach of file transfer protocol, or otherwise intercepting a transmission
+-- does not constitute a legal license or legal receipt of this code. Our code is transmitted to your device
+-- in order to run the software as we dictate. Using this code for your own personal or commercial use without
+-- a legal license granted by us may be prosecuted to the fullest extent of law for willful copyright infringement.
+
+-- All Rights Reserved.
+-- This material may not be modified, stored, published, rewritten, sold, redistributed, duplicated or reproduced
+-- in whole or in part without the express written permission with a lawful license for use granted thereof.
+
+START OF OUR CODE, THIS IS ONLY SUPPOSED TO BE FOR ITS INTENDED USE, ANY FORM OF COPYING WILL BE PURSUED TO THE FULLEST EXTENT OF COPYRIGHT LAW. HAVE A NICE DAY :)
+ */
 import React, { useState, useEffect } from 'react'
 import {
 	Text,
